c4491d6755d01872cb6b07f7659b23f0ac7c6378
SHan <hs04111@naver.com>
2021-11-25 23:11:08 +0900

    typescript: event type of form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import Circle from "./Circle";
-
-
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {currentTarget:{value}} = event;
+    setValue(value)
+  }
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`Hello, ${value}`)
+  }
+
   return (
-<div>
-  <Circle bgColor="teal" borderColor="yellow"/>
-  <Circle bgColor="tomato"/>
-</div>
+<form onSubmit={onSubmit}>
+<input type="text" placeholder="Username" onChange={onChange} value={value} />
+<button>Log In</button>
+</form>
   );
 }
 
