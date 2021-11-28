7a66416cf220fe647afb5b7da9f438f658d9e57a
SHan <hs04111@naver.com>
2021-11-28 12:25:00 +0900

    coin tracker: coin screen developed

--- src/Routes/Coin.tsx
+++ src/Routes/Coin.tsx
@@ -1,6 +1,15 @@
-import { useState } from 'react';
-import { useLocation, useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import {
+  Route,
+  Switch,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from 'react-router';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Chart from './Chart';
+import Price from './Price';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -24,6 +33,176 @@ const Loader = styled.span`
   display: block;
 `;
 
+const Overview = styled.div`
+  padding: 0px 20px;
+  margin: 0 auto;
+  max-width: 600px;
+  background-color: ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 7px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 5px;
+  span {
+    padding: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: ${(props) => props.theme.textColor};
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.bgColor};
+  padding: 7px 0px;
+  border-radius: 10px;
+
+  a {
+    display: block;
+  }
+`;
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  contract: string;
+  platform: string;
+  contracts: Contract[];
+  parent: Parent;
+  tags: Tag[];
+  team: Team[];
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: Date;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: Links;
+  links_extended: LinksExtended[];
+  whitepaper: Whitepaper;
+  first_data_at: Date;
+  last_data_at: Date;
+}
+
+interface Contract {
+  contract: string;
+  platform: string;
+  type: string;
+}
+
+interface Links {
+  explorer: string[];
+  facebook: string[];
+  reddit: string[];
+  source_code: string[];
+  website: string[];
+  youtube: string[];
+}
+
+interface LinksExtended {
+  url: string;
+  type: string;
+  stats?: Stats;
+}
+
+interface Stats {
+  subscribers?: number;
+  contributors?: number;
+  stars?: number;
+  followers?: number;
+}
+
+interface Parent {
+  id: string;
+  name: string;
+  symbol: string;
+}
+
+interface Tag {
+  id: string;
+  name: string;
+  coin_counter: number;
+  ico_counter: number;
+}
+
+interface Team {
+  id: string;
+  name: string;
+  position: string;
+}
+
+interface Whitepaper {
+  link: string;
+  thumbnail: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: Date;
+  last_updated: Date;
+  quotes: Quotes;
+}
+
+interface Quotes {
+  USD: Usd;
+}
+
+interface Usd {
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_15m: number;
+  percent_change_30m: number;
+  percent_change_1h: number;
+  percent_change_6h: number;
+  percent_change_12h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_1y: number;
+  ath_price: number;
+  ath_date: Date;
+  percent_from_price_ath: number;
+}
+
 interface paramsType {
   coinId: string;
 }
@@ -36,12 +215,79 @@ function Coin() {
   const { coinId } = useParams<paramsType>();
   const { state } = useLocation<routeState>();
   const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useRouteMatch('/:coinId/price');
+  const chartMatch = useRouteMatch('/:coinId/chart');
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
-        <Title>{state?.name || 'Loading...'}</Title>
+        <Title>
+          {state ? state.name : loading ? 'Loading...' : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>RANK:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>SYMBOL:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>OPEN SOURCE:</span>
+              <span>{info?.open_source ? 'YES' : 'NO'}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>TOTAL SUPPLY:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>MAX SUPPLY:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+          <Switch>
+            <Route path={`/${coinId}/price`}>
+              <Price />
+            </Route>
+            <Route path={`/${coinId}/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
     </Container>
   );
 }
