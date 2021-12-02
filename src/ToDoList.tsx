0a29f97e725594fd7940397bdab5ac4b6ef97cf1
SHan <hs04111@naver.com>
2021-12-02 21:31:31 +0900

    ToDo List: react hook form practice

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -28,17 +28,56 @@ import { useForm } from 'react-hook-form';
 //   );
 // }
 
+interface IForm {
+  Email: string;
+  toDo: string;
+  FirstName: string;
+  LastName?: string;
+  Password: string;
+  PasswordConfirm: string;
+  extraErr?: string;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
-  const onValid = (data: any) => {
-    console.log(data);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      Email: '@naver.com',
+    },
+  });
+  const onValid = (data: IForm) => {
+    if (data.Password !== data.PasswordConfirm) {
+      setError(
+        'PasswordConfirm',
+        { message: 'Passwords are not the same' },
+        { shouldFocus: true },
+      );
+    }
   };
-  console.log(formState.errors);
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input
-          {...register('toTo', {
+          {...register('Email', {
+            required: 'Email required',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: 'Only naver mail',
+            },
+          })}
+          placeholder="Email"
+          type="text"
+        />
+        <span>{errors?.Email?.message}</span>
+        <input
+          {...register('toDo', {
             required: 'ToDo is required',
             minLength: {
               value: 5,
@@ -48,8 +87,61 @@ function ToDoList() {
           placeholder="Write a ToDo"
           type="text"
         />
-        <input {...register('FirstName')} placeholder="FirstName" type="text" />
-        <input {...register('LastName')} placeholder="LastName" type="text" />
+        <span>{errors?.toDo?.message}</span>
+        <input
+          {...register('FirstName', {
+            required: 'Write here',
+            minLength: {
+              value: 3,
+              message: 'Min length 3',
+            },
+            validate: {
+              noNico: (value) =>
+                value.includes('nico') ? 'no nico allowed' : true,
+              noNick: (value) =>
+                value.includes('nick') ? 'no nick allowed' : true,
+            },
+          })}
+          placeholder="FirstName"
+          type="text"
+        />
+        <span>{errors?.FirstName?.message}</span>
+        <input
+          {...register('LastName', {
+            minLength: {
+              value: 3,
+              message: 'Min length 3',
+            },
+          })}
+          placeholder="LastName"
+          type="text"
+        />
+        <span>{errors?.LastName?.message}</span>
+
+        <input
+          {...register('Password', {
+            minLength: {
+              value: 3,
+              message: 'Min length 3',
+            },
+          })}
+          placeholder="Password"
+          type="password"
+        />
+        <span>{errors?.Password?.message}</span>
+
+        <input
+          {...register('PasswordConfirm', {
+            minLength: {
+              value: 3,
+              message: 'Min length 3',
+            },
+          })}
+          placeholder="PasswordConfirm"
+          type="password"
+        />
+        <span>{errors?.PasswordConfirm?.message}</span>
+        <span>{errors?.extraErr?.message}</span>
         <button>Submit</button>
       </form>
     </div>
