76d17f5b8fd723059fff13956d2b9080309350a3
SHan <hs04111@naver.com>
2021-12-02 23:25:32 +0900

    ToDo List: update categoy of ToDo

--- src/Components/ToDo.tsx
+++ src/Components/ToDo.tsx
@@ -1,12 +1,45 @@
-import { IToDo } from '../atoms';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDosState } from '../atoms';
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDosState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo: { text: string; id: number; category: any } = {
+        text,
+        id,
+        category: name,
+      };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== 'DOING' && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== 'TO_DO' && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== 'DONE' && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
