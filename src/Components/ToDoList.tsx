67ead27a29dd5a47d9d396e892a0ba02ed84c4d2
SHan <hs04111@naver.com>
2021-12-12 10:52:50 +0900

    To Do list: recoil selector basics

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import { toDosState } from '../atoms';
+import { toDoSelector, toDosState } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDosState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <CreateToDo />
+      <h1>To do</h1>
       <ul>
-        {toDos.map((todo) => (
+        {toDo.map((todo) => (
           <ToDo key={todo.id} {...todo} />
         ))}
       </ul>
+      <hr />
+      <h1>Doing</h1>
+      <ul>
+        {doing.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
+      <h1>Done</h1>
+      <ul>
+        {done.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,16 @@ export interface IToDo {
 export const toDosState = atom<IToDo[]>({
   key: 'toDoList',
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: 'toDoSelector',
+  get: ({ get }) => {
+    const toDos = get(toDosState);
+    return [
+      toDos.filter((toDo) => toDo.category === 'TO_DO'),
+      toDos.filter((toDo) => toDo.category === 'DOING'),
+      toDos.filter((toDo) => toDo.category === 'DONE'),
+    ];
+  },
 });
